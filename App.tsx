55a13fba20a860bdac061fa2efa9a9a6d5982492
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2024-02-04 15:32:57 -0300

    Criando o componente de loading

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
-import { ActivityIndicator } from "react-native";
-
 import { ThemeProvider } from "styled-components";
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
 
 import theme from "@theme/index";
 import { Home } from "@screens/Home";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Home /> :  <ActivityIndicator />}
+      { fontsLoaded ? <Home /> :  <Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,14 @@
+import theme from "@theme/index";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${theme.COLORS.WHITE};
+`;
+
+export const LoadIndicator = styled.ActivityIndicator.attrs({
+  color: theme.COLORS.GREY_2,
+})``;

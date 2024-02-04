f0f813e50594f6b746301e3fc411ae90bcd81294
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2024-02-04 15:17:28 -0300

    Adicionando a fonte

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
+import { ActivityIndicator } from "react-native";
+
 import { ThemeProvider } from "styled-components";
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
 
 import theme from "@theme/index";
 import { Home } from "@screens/Home";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      { fontsLoaded ? <Home /> :  <ActivityIndicator />}
     </ThemeProvider>
   );
 }

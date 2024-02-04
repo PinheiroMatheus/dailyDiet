6ee1847695d73ba9c818e296787a3d6fa626f096
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2024-02-04 14:15:34 -0300

    Utilizando o styled components

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    width: 100%;
+    background-color: #000;
+    align-items: 'center';
+    justify-content: 'center';
+`;
+
+export const Title = styled.Text`
+    color: #FFF;
+    font-size: 32;
+`;

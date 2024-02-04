ff4cf973374ae8b64e74b9377f6c8909cb24a580
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2024-02-04 18:20:35 -0300

    Retirando o titulo da página

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,12 +3,6 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: ${ theme.COLORS.WHITE };
-`;
 
-export const Title = styled.Text`
-    color: ${ theme.COLORS.GREEN_DARK };
-    font-size: 32px;
+    background-color: ${ theme.COLORS.WHITE };
 `;

8869ae3811260051bfbf97250f4dd2139bbb70b0
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2024-02-04 14:59:24 -0300

    Corrigindo a utilizacao dos temas

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,12 @@
+import theme from "@theme/index";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${({ theme }) => theme.COLORS.WHITE};
+    background-color: ${ theme.COLORS.WHITE };
 `;
 
 export const Title = styled.Text`
-    color: ${({ theme }) => theme.COLORS.RED_DARK};
-    font-size: 32;
+    color: ${ theme.COLORS.GREEN_DARK };
+    font-size: 32px;
 `;
